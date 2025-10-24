580ad3007344dc2f097b63ff9ab9771c9dc11bd2
Ningombam Borish Singh <131790729+BORISH1@users.noreply.github.com>
2025-10-24 15:27:01 +0530

    borish

--- src/main/java/borish/printingHelloWorld.java
+++ src/main/java/borish/printingHelloWorld.java
@@ -4,6 +4,8 @@ public class printingHelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("Hello World");	}
+ System.out.println("Hello World");	
+ System.out.println("This is the simple code");
+	}
 
 }
